3a2c9381648f55922d6804f39c651c633bcf377c
marcos <marcoslucero@ceroca.cat>
2025-02-12 17:53:19 +0100

    Firebase installado con el path

--- lib/main.dart
+++ lib/main.dart
@@ -18,3 +18,29 @@ class MainApp extends StatelessWidget {
     );
   }
 }
+
+
+/*
+1) tener node.js instalado
+  - Comprobar con:
+    node -v
+    npm -v
+
+2) Pagina de firebase; go to console
+  - Cuenta de google
+
+3) Crear un nuevo proyecto
+  - Nombre del proyecto
+
+4) Ir al menu compilacion y habilitar: 
+    Autenticacion por correo electronico y firestore database
+
+5) en cmd de VScode:
+    npm install -g firebase-tools en viusal o cmd (instalar firebase tools)
+    poner firebase login y usar el mismo correo de google de antes
+
+6) en terminal del viusal 
+  -Flutter pub global activate flutterfire_cli 
+  -dart pub global activate flutterfire_cli
+    -flutterfire configure
+*/
